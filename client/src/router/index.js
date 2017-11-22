6aacd93388cd2eaf773148a6803a2b7ff58a84ee
allanac <acole006@fiu.edu>
2017-11-22 18:42:01 -0500

    init the vuejs application using vue cli

---
+++ client/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+Vue.use(Vuetify)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
