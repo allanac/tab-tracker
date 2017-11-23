5c5466eb50165249456b8d0a925a296ce78f8ad3
allanac <acole006@fiu.edu>
2017-11-22 21:41:39 -0500

    added register component and API to grab data that works with backend

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 Vue.use(Vuetify)
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
