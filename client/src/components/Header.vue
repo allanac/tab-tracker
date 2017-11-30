e549a17598ced4dd54710fa57f30ae77e4e189ba
allanac <acole006@fiu.edu>
2017-11-29 23:29:40 -0500

    added logout button

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -38,6 +38,17 @@
         </v-btn>
     </v-toolbar-items>
 
+    <!-- Logout Button  -->
+    <v-toolbar-items>
+        <v-btn
+          v-if="$store.state.isUserLoggedIn"
+          flat
+          dark
+          @click="logout">
+          Log Out
+        </v-btn>
+    </v-toolbar-items>
+
   </v-toolbar>
 </template>
 
@@ -46,6 +57,13 @@ export default{
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name:'root'
+      })
     }
   }
 }
