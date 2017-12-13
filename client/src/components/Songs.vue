2716fc437c21c964b2b59b327e7488f6670784bb
allanac <acole006@fiu.edu>
2017-12-12 23:40:50 -0500

    added panel and songs compenents, utilising slots

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -1,23 +1,25 @@
 <template lang="html">
   <v-layout column>
     <v-flex xs6 offset-xs3>
-      <div class="white elevation-2">
-        <v-toolbar flat dense color="cyan" class="white--text">
-          <v-toolbar-title> Songs </v-toolbar-title>
-        </v-toolbar>
 
-      <div class="pl-4 pr-4 pb-2 pt-2"></div>
-        <div class="error" v-html="error"/>
-      </div>
-    </div>
+      <panel title="Songs">
+      </panel>
+      
     </v-flex>
   </v-layout>
 </template>
 
+
 <script>
+import Panel from '@/components/Panel'
 export default {
+  components: {
+    Panel
+  }
 }
 </script>
+
+
 
 <style lang="css">
 </style>
