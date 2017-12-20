7a54b407f5fe02e846a508e9779a2ca0b78c15e9
allanac <acole006@fiu.edu>
2017-12-19 23:55:31 -0500

    added script for Creating Songs

---
+++ client/src/components/CreateSong.vue
@@ -0,0 +1,54 @@
+<template>
+
+</template>
+
+
+
+
+<script>
+export default {
+  data () {
+    return {
+      song:{
+        title: null,
+        artist: null,
+        genre: null,
+        album: null,
+        albumImageUrl: null,
+        youtubeId: null,
+        lyrics: null,
+        tab: null
+      },
+      error: null,
+      required: (value) => !!value || 'Required.'
+    }
+  },
+    methods: {
+      async create () {
+        this.error = null
+        const areAllFieldsFilledIn = Object
+          .keys(this.song)
+          .every(ket => !!this.song[key])
+        if(!areAllFieldsFilledIn){
+          this.error = 'Please fill in all the required fields.'
+          return
+        }
+        try {
+          await SongService.post(this.song)
+          this.$router.push({
+            name:'songs'
+          })
+        } catch (err){
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
+
+
+
+
+
+<style lang="css">
+</style>
