4cc317f1b77e4f3e95fdb384a04f4046cd97b077
allanac <acole006@fiu.edu>
2017-12-22 23:54:06 -0500

    added search query for songs

--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -11,12 +11,32 @@ function jwtSignUser (user) {
 module.exports = {
   async index (req, res) {
     try {
-      
+      let songs = null
+      const search = req.query.search
+      if(search){
+        songs = await Song.findAll({
+          where: {
+            $or: [
+              'title', 'artist', 'genre', 'album'
+            ].map(key =>({
+              [key]: {
+                $like: `%${search}%`
+              }
+            }))
+          }
+        })
+      } else {
+        songs = await Song.findAll({
+          limit: 10
+        })
+      }
+      res.send(songs)
     } catch (err) {
        res.status(500).send({
         error: 'An error has occured trying to login'
       })
     }
-  }
+  },
+  
 
 }
