9d72fd2b28215b7a54b4a3208e2f437e8d43ef9d
allanac <acole006@fiu.edu>
2017-12-26 22:56:45 -0500

    completed create song compenent server and client

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -1,11 +1,90 @@
 <template>
+  <v-layout>
+    <v-flex xs4>
+      <panel title="Song Metadata">
+        <v-text-field
+          label="Title"
+          required
+          :rules="[required]"
+          v-model="song.title"
+        ></v-text-field>
 
+        <v-text-field
+          label="Artist"
+          required
+          :rules="[required]"
+          v-model="song.artist"
+        ></v-text-field>
+
+        <v-text-field
+          label="Genre"
+          required
+          :rules="[required]"
+          v-model="song.genre"
+        ></v-text-field>
+
+        <v-text-field
+          label="Album"
+          required
+          :rules="[required]"
+          v-model="song.album"
+        ></v-text-field>
+
+        <v-text-field
+          label="Album Image Url"
+          required
+          :rules="[required]"
+          v-model="song.albumImageUrl"
+        ></v-text-field>
+
+        <v-text-field
+          label="YouTube ID"
+          required
+          :rules="[required]"
+          v-model="song.youtubeId"
+        ></v-text-field>
+      </panel>
+    </v-flex>
+
+    <v-flex xs8>
+      <panel title="Song Structure" class="ml-2">
+        <v-text-field
+          label="Tab"
+          multi-line
+          required
+          :rules="[required]"
+          v-model="song.tab"
+        ></v-text-field>
+
+        <v-text-field
+          label="Lyrics"
+          multi-line
+          required
+          :rules="[required]"
+          v-model="song.lyrics"
+        ></v-text-field>
+      </panel>
+
+      <div class="danger-alert" v-if="error">
+        {{error}}
+      </div>
+
+      <v-btn
+        dark
+        class="cyan"
+        @click="create">
+        Create Song
+      </v-btn>
+    </v-flex>
+  </v-layout>
 </template>
 
 
 
 
 <script>
+import SongService from '@/servicdes/SongsService'
+
 export default {
   data () {
     return {
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Register from '@/components/Register'
 import Login from '@/components/Login'
 import Songs from '@/components/Songs'
+import CreateSong from '@/components/CreateSong'
 
 Vue.use(Router)
 
@@ -28,6 +28,11 @@ export default new Router({
       path: '/songs',
       name: 'songs',
       component: Songs
+    },
+    {
+      path: '/songs/create',
+      name: 'songs-create',
+      component: CreateSong
     }
   ]
 })
